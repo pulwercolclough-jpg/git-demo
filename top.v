07652e32ac144ce3f56d63f1ef40afaa67a96083
Huangichin <2425350338@qq.com>
2026-07-14 12:31:04 +0800

    Add basic top.v

--- top.v
+++ top.v
@@ -1,1 +1,13 @@
-module top();endmodule
+module top(
+
+
+# 1. 确保回到 main 分支
+git checkout main
+
+# 2. 写入一段包含复位信号的 Verilog 代码
+cat <<EOT > top.v
+module top(
+    input clk,
+    input rst_n
+);
+endmodule
